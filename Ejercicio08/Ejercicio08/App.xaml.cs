71c8b06cc704687de710e303d692e430d7a3861e
kevinbh168 <kevin.brena@tecsup.edu.pe>
2019-10-21 23:34:35 -0500

    lab10 terminado

---
+++ Ejercicio08/Ejercicio08/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Ejercicio08.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio08
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            MainPage = new Operaciones();
+            //MainPage = new Views.Operaciones();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Ejercicio08/Ejercicio08/ViewModels/OperacionesViewModel.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Ejercicio08.ViewModels
+{
+    public class OperacionesViewModel : ViewModelBase
+    {
+
+        #region Propiedades
+        int valor1;
+        public int Valor1
+        {
+            get { return valor1; }
+            set
+            {
+                if (valor1 != value)
+                {
+                    valor1 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int valor2;
+        public int Valor2
+        {
+            get { return valor2; }
+            set
+            {
+                if (valor2 != value)
+                {
+                    valor2 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int suma;
+        public int ResultSuma
+        {
+            get { return suma; }
+            set
+            {
+                if (suma != value)
+                {
+                    suma = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int resta;
+        public int ResultResta
+        {
+            get { return resta; }
+            set
+            {
+                if (resta != value)
+                {
+                    resta = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        double producto;
+
+        public double ResultProducto
+        {
+            get { return producto; }
+            set
+            {
+                if(producto != value)
+                {
+                    producto = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        double division;
+
+        public double ResultDividir
+        {
+            get { return division; }
+            set
+            {
+                if(division != value)
+                {
+                    division = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        #endregion
+
+        #region Comandos 
+        public ICommand Sumar { protected set; get; }
+
+        public ICommand Restar { protected set; get; }
+
+        public ICommand Multiplicar { protected set; get; }
+
+        public ICommand Dividir { protected set; get; }
+
+        #endregion
+
+        #region Constructor
+
+        public OperacionesViewModel()
+        {
+
+            Sumar = new Command(() =>
+            {
+                ResultSuma = Valor1 + Valor2;
+            });
+
+            Restar = new Command(() =>
+            {
+                ResultResta = Valor1 - Valor2;
+            });
+
+            Multiplicar = new Command(() =>
+            {
+                ResultProducto = Valor1 * Valor2;
+            });
+
+            Dividir = new Command(() =>
+            {
+                ResultDividir = Valor1 / Valor2;
+            });
+
+        }
+        #endregion
+
+    }
+}
